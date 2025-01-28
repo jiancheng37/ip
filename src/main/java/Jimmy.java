3a0da96c5023ad9f36b96b051d7b598d606756e3
Jian Cheng <lowjiancheng3773@gmail.com>
2025-01-28 15:21:49 +0800

    Gave my bot the ability to store user's text and display them when prompted

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Jimmy {
     public static void main(String[] args) {
+        String[] list = new String[100];
+        int index = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("    __________________________________________________");
         System.out.println("    Hello! I'm Jimmy. What can I do for you?");
@@ -14,9 +16,20 @@ public class Jimmy {
                 System.out.println("    __________________________________________________");
                 break;
             }
-            System.out.println("    __________________________________________________");
-            System.out.println("    " + input);
-            System.out.println("    __________________________________________________");
+            else if (input.equals("list")) {
+                System.out.println("    __________________________________________________");
+                for (int i = 0; i < index; i++) {
+                    System.out.println("    " + (i + 1) + ". " + list[i]);
+                }
+                System.out.println("    __________________________________________________");
+            }
+            else {
+                System.out.println("    __________________________________________________");
+                System.out.println("    added: " + input);
+                System.out.println("    __________________________________________________");
+                list[index] = input;
+                index++;
+            }
         }
         sc.close();
     }
