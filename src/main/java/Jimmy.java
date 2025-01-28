9cd958f9d2d61aabb9daee5a85525f330b262179
Jian Cheng <lowjiancheng3773@gmail.com>
2025-01-28 14:47:57 +0800

    Echos user commands and exits once user types bye

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Jimmy {
     public static void main(String[] args) {
-        System.out.println("Hello from Jimmy!");
-        System.out.println("Goodbye!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    __________________________________________________");
+        System.out.println("    Hello! I'm Jimmy. What can I do for you?");
+        System.out.println("    __________________________________________________");
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println("    __________________________________________________");
+            System.out.println("    " + input);
+            System.out.println("    __________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    __________________________________________________");
+                break;
+            }
+        }
+        sc.close();
     }
 }
