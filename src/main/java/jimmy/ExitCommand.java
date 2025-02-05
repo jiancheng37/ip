e0dc0837e3df0984b0d8e9306340ab618a136873
Jian Cheng <lowjiancheng3773@gmail.com>
2025-02-05 13:08:57 +0800

    Implement Gui

--- src/main/java/jimmy/ExitCommand.java
+++ src/main/java/jimmy/ExitCommand.java
@@ -20,7 +20,7 @@ public class ExitCommand extends Command {
      */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        ui.showGoodbye();
+        return;
     }
 
     /**
